68629009d78792fe415e907a3b56ad3fedcbc710
Владислав Кандауров <v.kandaurov@els24.com>
2022-03-23 22:31:42 +0700

    wip(home): add home screen header

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,97 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     StyleSheet,
     View,
     Text,
+    ScrollView,
+    FlatList,
+    Image,
+    ImageBackground,
     TouchableOpacity
 } from 'react-native';
+import {dummyData, COLORS, SIZES, FONTS, icons, images} from '../constants'
 
 const Home = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("CryptoDetail")}
+
+    const [trending] = useState(dummyData.trendingCurrencies)
+
+const renderHeader = ()=>{
+
+
+    const renderItem = ({item, index})=>{
+        return(
+            <TouchableOpacity style={{
+                width:180, 
+                padding:SIZES.padding, 
+                marginLeft: index==0? SIZES.padding: 0, 
+                marginRight:SIZES.radius, 
+                borderRadius:10, 
+                backgroundColor: COLORS.white }}
             >
-                <Text>Navigate to CryptoDetail</Text>
+                <View style={{flexDirection:'row'}}>
+                    <View>
+                        <Image source={item.image} resizeMode="cover" style={{marginTop:5, width:25, height:25}}/>
+                    </View>
+                    <View style={{marginLeft:SIZES.base}}>
+                        <Text style={{...FONTS.h2}}>{item.currency}</Text>
+                        <Text style={{color: COLORS.gray, ...FONTS.body3}}>{item.code}</Text>
+                    </View>
+                </View>
+
+                <View style={{marginTop: SIZES.radius}}>
+                    <Text style={{...FONTS.h2}}>${item.amount}</Text>
+                    <Text style={{color: item.type == "I" ? COLORS.green: COLORS.red, ...FONTS.h3}}>{item.changes}</Text>
+                </View>
             </TouchableOpacity>
+        )
+    }
+
+    return (
+        <View style={{
+            width:"100%",
+            height:290,
+            ...styles.shadow,
+        }}>
+            <ImageBackground source={images.banner} resizeMode="cover" style={{flex:1, alignItems:'center'}}>
+                <View style={{marginTop: SIZES.padding*2,  width:"100%", alignItems:'flex-end', paddingHorizontal: SIZES.padding}}>
+                    <TouchableOpacity style={{width:35, height:35, alignItems:'center', justifyContent:'center'}} onPress={()=>console.log('Notification on press')}>
+                        <Image
+                        source={icons.notification_white}
+                        resizeMode="contain"
+                        style={{flex:1}}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{alignItems:'center', justifyContent:'center'}}>
+                    <Text style={{color: COLORS.white, ...FONTS.h3}}>Your Portfolio Balance</Text>
+                    <Text style={{marginTop: SIZES.base, color: COLORS.white, ...FONTS.h1}}>${dummyData.portfolio.balance}</Text>
+                    <Text style={{color:COLORS.white, ...FONTS.body5}}>{dummyData.portfolio.changes} Last 24 hours</Text>
+                </View>
+
+                <View style={{position:'absolute', bottom: "-30%"}}>
+                    <Text style={{marginLeft: SIZES.padding, color:COLORS.white, ...FONTS.h2}}>Trending</Text>
+                    <FlatList 
+                    contentContainerStyle={{marginTop: SIZES.base}}
+                    data={trending}
+                    renderItem={renderItem}
+                    keyExtractor={item=>`${item.id}`}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    />
+                </View>
+
+            </ImageBackground>
         </View>
+    )
+}
+
+    return (
+        <ScrollView>
+            <View style={{flex:1, paddingBottom: 130 }}>
+              {renderHeader()}
+            </View>
+        </ScrollView>
     )
 }
 
