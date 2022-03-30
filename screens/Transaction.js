20f6fba3222bd3416edeb931397ce40c89778b79
vladkandaurov <vlad_kandaurov@mail.ru>
2022-03-30 21:14:49 +0700

    feat: add transaction history screen

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,15 +1,71 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import {
     StyleSheet,
+    SafeAreaView,
+    ScrollView,
     View,
     Text
 } from 'react-native';
 
-const Transaction = () => {
+
+import {HeaderBar,CurrencyLabel,TextButton, TransactionHistory} from "../components"
+import {FONTS, SIZES, dummyData, COLORS} from "../constants"
+
+const Transaction = ({route}) => {
+    console.log(route)
+
+    const[selectedCurrency, setSelectedCurrency]=useState(null)
+    useEffect(() => {
+      const {currency} = route.params;
+      setSelectedCurrency(currency)
+    }, [])
+    
+
+const renderTrade = ()=>{
     return (
-        <View style={styles.container}>
-            <Text>Transaction</Text>
+        <View style={{
+            marginTop:SIZES.padding,
+            marginHorizontal:SIZES.padding,
+            padding:SIZES.padding,
+            borderRadius:SIZES.radius,
+            backgroundColor:COLORS.white,
+            ...styles.shadow
+            }}>
+                <CurrencyLabel icon={selectedCurrency?.image} currency={selectedCurrency?.currency} code={selectedCurrency?.code}/>
+                <View style={{
+                      marginTop:SIZES.padding,
+                      marginBottom:SIZES.padding *1.5,
+                      alignItems:'center',
+                      justifyContent:'center'
+                }}>
+                    <Text style={{...FONTS.h2}}>{selectedCurrency?.wallet.crypto} {selectedCurrency?.code}</Text>
+                    <Text style={{color:COLORS.gray, ...FONTS.body4}}>${selectedCurrency?.wallet.value}</Text>
+
+                </View>
+                <TextButton label={"Trade"}/>
+
         </View>
+    )
+}
+
+const renderTransactionHistory =()=>{
+    return (
+        <TransactionHistory customContainerStyle={{...styles.shadow}} history={selectedCurrency?.transactionHistory}/>
+    )
+}
+
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <HeaderBar right={false}/>
+            <ScrollView>
+                <View style={{flex:1, paddingBottom:SIZES.padding}}>
+                    {renderTrade()}
+                    {renderTransactionHistory()}
+
+                </View>
+            </ScrollView>
+
+        </SafeAreaView>
     )
 }
 
